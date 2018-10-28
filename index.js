ede5e869f833ce7597d647360ff0fd9e9ce826a9
Raheem <raheemtheallstar137@yahoo.com>
2018-10-28 16:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var animal  = cat;
-  var result = myAnimal();
-  return result;
+  myAnimal();
 }
 
 function add2(n) {
